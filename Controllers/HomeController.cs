4501e90238af04972619b07bd6e860fc8614c642
RedzCafe <software@devoredev.com>
2022-07-09 20:18:32 -0400

    round 1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCollectionSite.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+using System.IO;
+
+namespace MyCollectionSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HatRepository repository;
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            HatRepository repository)
+        {
+            _logger = logger;
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+
+        {
+            //1st run, before repository created
+            // using var jsonFile = System.IO.File.OpenRead("Data/collection.json");
+            //var items = JsonSerializer.Deserialize<CollectionItem[]>(jsonFile);
+
+            var items = repository.Get();
+            return View(items);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HatRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace MyCollectionSite.Models
+{
+    public class HatRepository
+    {
+        public IEnumerable<CollectionItem> Get()
+        {
+            using var jsonFile = System.IO.File.OpenRead("Data/collection.json");
+            return JsonSerializer.Deserialize<CollectionItem[]>(jsonFile);
+        }
+    }
+}
